ebe1e4d71ceb4a22585dab78b224f328d7306898
MAlharbi-dev <Msaadcs1@gmail.com>
2025-04-03 11:30:58 +0300

    Lab 5: Links and Layout Customization

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 import apps.bookmodule.views
 
@@ -22,5 +23,10 @@ import apps.bookmodule.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.bookmodule.urls")), 
-    path('users/', include("apps.usermodule.urls")) 
+    path('users/', include("apps.usermodule.urls")),
+    path('books/html5/links/', lambda request: render(request, 'books/html5/links/links.html'), name='html5-links'),
+    path('books/html5/text/formatting', lambda request: render(request, 'books/html5/text/formatting/formatting.html'), name='html5-links'),
+    path('books/html5/text/listing', lambda request: render(request, 'books/html5/text/listing/listing.html'), name='html5-links'),
+    path('books/html5/tables/', lambda request: render(request, 'books/html5/tables/tables.html'), name='html5-links'),
+    
 ]
